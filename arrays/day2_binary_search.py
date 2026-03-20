b893c8b9ddf2e1a8ba82c757192ca74dd33dc1b3
hrushi1305 <hrushisaini@gmail.com>
2026-03-20 19:58:17 +0530

    day 2 binary search complete with advanced problems

--- arrays/day2_binary_search.py
+++ arrays/day2_binary_search.py
@@ -41,9 +41,35 @@ def last_occurrence(arr, target):
             right=mid-1
     return result
 
+def count_occurrences(arr, target):
+    first=first_occurrence(arr, target)
+    if first==-1:
+        return 0
+    last=last_occurrence(arr, target)
+    return last-first+1
+
+def sqrt_binary(n):
+    left=0
+    right=n
+    ans=0
+    while left<=right:
+        mid=left+right//2
+        if mid*mid==n:
+            return mid
+        elif mid*mid<n:
+            ans=mid
+            left=mid+1
+            
+        else:
+            right=mid-1
+    return ans
+        
 # Example usage:
 arr = [1, 2, 3, 4, 5, 5, 5, 6, 7]
 target = 5
 print("Binary Search:", binary_search(arr, target))  # Output: Index of target (could be any of the indices where 5 is located)
 print("First Occurrence:", first_occurrence(arr, target))  # Output: Index of the first occurrence of target (4)
-print("Last Occurrence:", last_occurrence(arr, target))  # Output: Index of the last
+print("Last Occurrence:", last_occurrence(arr, target))  # Output: Index of the last occurrence of target (6)
+print("Count Occurrences:", count_occurrences(arr, target))  # Output: Count of target in the array (3)
+print("Square Root:", sqrt_binary(16))  # Output: 4
+print("Square Root:", sqrt_binary(20))  # Output: 4
