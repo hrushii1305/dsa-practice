f13a5bf9195d6491eee4a5cf5482ecc738b49f34
hrushi1305 <hrushisaini@gmail.com>
2026-03-23 20:20:50 +0530

    updated day 5 with 3 problems

--- arrays/day5_hashing_two_pointers.py
+++ arrays/day5_hashing_two_pointers.py
@@ -18,3 +18,16 @@ def is_anagram(s,t):
             return False
         count[ch]-=1
     return True
+
+def pair_with_target(arr,target):
+    left=0
+    right=len(arr)-1
+    while left<right:
+        current_sum=arr[left]+arr[right]
+        if current_sum==target:
+            return [left,right]
+        elif current_sum<target:
+            left+=1
+        else:
+            right-=1
+    return []
