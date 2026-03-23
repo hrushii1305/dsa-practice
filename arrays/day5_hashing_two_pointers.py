6970bae3bbb28c7612d977a4b02163fe7a12b107
hrushi1305 <hrushisaini@gmail.com>
2026-03-23 20:18:04 +0530

    completed 2 problems on day 5

---
+++ arrays/day5_hashing_two_pointers.py
@@ -0,0 +1,20 @@
+def two_sum(nums,target):
+    num_map = {}
+    for i in range(len(nums)):
+        diff=target-nums[i]
+        if diff in num_map:
+            return [num_map[diff],i]
+        num_map[nums[i]]=i
+    return []
+
+def is_anagram(s,t):
+    if len(s)!=len(t):
+        return False
+    count={}
+    for ch in s:
+        count[ch]=count.get(ch,0)+1
+    for ch in t:
+        if ch not in count or count[ch]==0:
+            return False
+        count[ch]-=1
+    return True
