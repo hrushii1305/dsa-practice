2c992c526de978b5e35e079bec9391b4610877db
hrushi1305 <hrushisaini@gmail.com>
2026-03-27 19:24:04 +0530

    updated day 1 week 4 two pointers

--- arrays/week4_day1_two_pointers.py
+++ arrays/week4_day1_two_pointers.py
@@ -14,7 +14,28 @@ class twopointers:
                 right -= 1
 
         return None  # Return None if no pair is found
-
+    
+    
+    def remove_duplicates(self,nums):
+        if not nums:
+            return []
+        slow=0
+        for fast in range(1,len(nums)):
+            if nums[slow]!=nums[fast]:
+                slow+=1
+                nums[slow]=nums[fast]
+        return nums[:slow+1]
+    
+    def move_zeros(self,nums):
+        slow=0
+        for fast in range(len(nums)):
+            if nums[fast]!=0:
+                nums[slow],nums[fast]=nums[fast],nums[slow]
+                slow+=1
+        return nums
+    
 
 obj=twopointers()
 print(obj.pair_sum([1, 2, 3, 4, 5], 9))  # Output: [3, 4]
+print(obj.remove_duplicates([1, 1, 2, 2, 3, 3]))  # Output: [1, 2, 3]
+print(obj.move_zeros([0, 1, 0, 3, 12]))  # Output: [1, 3, 12, 0, 0]
