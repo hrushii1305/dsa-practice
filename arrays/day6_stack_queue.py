a969361cf670a2f5bfda98f42ed8052ea08a5806
hrushi1305 <hrushisaini@gmail.com>
2026-04-07 19:33:16 +0530

    updated day 6 with stack programs

--- arrays/day6_stack_queue.py
+++ arrays/day6_stack_queue.py
@@ -1,14 +1,65 @@
-def is_valid(s):
-    stack=[]
-    mapping={")":"(","}":"{","]":"["}
-    for ch in s:
-        if ch in mapping:
-            if stack and stack[-1]==mapping[ch]:
+class stack:
+    def __init__(self):
+        self.stack=[]
+    def push(self,x):
+        self.stack.append(x)
+    def pop(self):
+        if not self.is_empty():
+            return self.stack.pop()
+        return "stack is empty"
+    def peek(self):
+        if not self.is_empty():
+            return self.stack[-1]
+        return "stack is empty"
+    def is_empty(self):
+        return len(self.stack)==0
+    
+    
+obj=stack()
+obj.push(1)
+obj.push(2)
+obj.push(3)
+obj.push(4)
+print(obj.peek())
+print(obj.pop())
+print(obj.peek())
+
+class solution:
+    def is_valid(self,s):
+        stack=[]
+        mapping={')':'(',']':'[','}':'{'}
+        for ch in s:
+            if ch in mapping:
+                if not stack or stack[-1]!=mapping[ch]:
+                    return False
                 stack.pop()
             else:
-                return False
-        else:
-            stack.append(ch)
-    return True if not stack else False
-
-print(is_valid("()[]{}"))
+                stack.append(ch)
+        return len(stack)==0
+    
+    def next_greater(self,nums):
+        res=[-1]*len(nums)
+        stack=[]
+        for i in range(len(nums)):
+            while stack and nums[i]>nums[stack[-1]]:
+                index=stack.pop()
+                res[index]=nums[i]
+            stack.append(i)
+        return res
+    
+    def next_smaller(self,nums):
+        res=[-1]*len(nums)
+        stack=[]
+        for i in range(len(nums)):
+            while stack and nums[i]<nums[stack[-1]]:
+                index=stack.pop()
+                res[index]=nums[i]
+            stack.append(i)
+        return res
+    
+obj=solution()
+s="()[]{}"
+print(obj.is_valid(s))  # Output: True
+nums=[4,5,2,10,8]
+print(obj.next_greater(nums))  # Output: [5, 10, 10, -1, -1]
+print(obj.next_smaller(nums))  # Output: [2, 2, -1, 8, -1]
